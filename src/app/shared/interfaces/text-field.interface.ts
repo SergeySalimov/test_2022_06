0937ca0b6a6f63387a97249d2ad935a09899416f
Sergey Salimov <sergey.salimov@intexsoft.by>
2022-05-27 10:39:30 +0300

    ITX7OTHR-17 added parser for text-field.interface.ts

---
+++ src/app/shared/interfaces/text-field.interface.ts
@@ -0,0 +1,30 @@
+export interface TextFieldInterface {
+  common: {
+    add: string;
+    save: string;
+    cancel: string;
+  },
+  todo: {
+    logo: string;
+    inputPlaceholder: string;
+    addToDo: string;
+    noDataInTable: string;
+    tableDate: string;
+    tableDescription: string;
+    tableAction: string;
+  },
+  card: {
+    cardHeaderText: string;
+    noCardText: string;
+    createdAt: string;
+    surname: string;
+    name: string;
+    patronymic: string;
+    city: string;
+    phone: string;
+    zipCode: string;
+    email: string;
+    address: string;
+    notes: string;
+  },
+}
